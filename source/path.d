9bd1add12220c288c7106297155d042560ab67d0
Animus PEXUS <animuspexus@protonmail.com>
2021-01-02 19:25:30 +0300

    update

--- source/path.d
+++ source/path.d
@@ -1,9 +1,227 @@
 module dutils.path;
 
+import std.path;
+import std.algorithm;
+import std.array;
+
+import dutils.string;
+
+// TODO: make more unittests? 
+
+const doubleDirSeparator = dirSeparator ~ dirSeparator;
+
+private string[] deleteEmptyItems(string[] args)
+{
+    // TODO: maybe add searchin and splitting by separator in args items
+    string[] ret;
+    foreach (i; args)
+    {
+        if (i == "")
+        {
+            continue;
+        }
+        ret ~= i;
+    }
+    return ret;
+}
+
+unittest
+{
+    import std.stdio;
+
+    auto t1 = deleteEmptyItems(cast(string[])["", "a", "b", "", "c"]);
+    assert(t1 == cast(string[])["a", "b", "c"]);
+}
+
+private string deleteEmptyItems(string value)
+{
+
+    value = value.replace(doubleDirSeparator, dirSeparator);
+
+    return value;
+}
+
+string[] split(string value, bool keep_end_separator = false)
+{
+    auto start_separator = startsWithSeparator(value);
+    auto end_separator = endsWithSeparator(value);
+
+    value = deleteEmptyItems(value);
+    auto splitted = dutils.string.split(value, dirSeparator);
+    splitted = deleteEmptyItems(splitted);
+
+    if (start_separator)
+    {
+        splitted = (cast(string[])[""]) ~ splitted;
+    }
+
+    if (keep_end_separator && end_separator)
+    {
+        splitted ~= "";
+    }
+
+    return splitted;
+}
+
+unittest
+{
+    auto t1 = split("/a/b/c");
+    assert(t1 == cast(string[])["", "a", "b", "c"]);
+
+    t1 = split("/a/b/c/", true);
+    assert(t1 == cast(string[])["", "a", "b", "c", ""]);
+
+    t1 = split("a/b/c");
+    assert(t1 == cast(string[])["a", "b", "c"]);
+
+    t1 = split("a/b/c/", true);
+    assert(t1 == cast(string[])["a", "b", "c", ""]);
+
+    t1 = split("a/b/c//", true);
+    assert(t1 == cast(string[])["a", "b", "c", ""]);
+}
+
+string[] split(string[] args, bool keep_end_separator = false)
+{
+
+    auto start_separator = startsWithSeparator(args);
+    auto end_separator = endsWithSeparator(args);
+
+    string[] splitted;
+
+    foreach (item; args)
+    {
+        splitted ~= split(item, false);
+    }
+
+    splitted = deleteEmptyItems(splitted);
+
+    if (start_separator)
+    {
+        splitted = (cast(string[])[""]) ~ splitted;
+    }
+
+    if (keep_end_separator && end_separator)
+    {
+        splitted ~= "";
+    }
+
+    return splitted;
+}
+
+unittest
+{
+    auto t1 = split(["/a", "//", "", "b", "/c"]);
+    assert(t1 == cast(string[])["", "a", "b", "c"]);
+
+    t1 = split(["d/a", "//", "", "b", "/c"]);
+    assert(t1 == cast(string[])["d", "a", "b", "c"]);
+
+    t1 = split(["d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == cast(string[])["d", "a", "b", "c"]);
+
+    t1 = split(["d/a", "//", "", "b", "/c", ""], true);
+    assert(t1 == ["d", "a", "b", "c", ""]);
+}
+
+bool startsWithSeparator(string value)
+{
+    return value.startsWith(dirSeparator);
+}
+
+bool startsWithSeparator(string[] args)
+{
+    return (args.length != 0 && (args[0] == "" || args[0].startsWith(dirSeparator)));
+}
+
+unittest
+{
+    auto t1 = startsWithSeparator(["/a", "//", "", "b", "/c"]);
+    assert(t1 == true);
+
+    t1 = startsWithSeparator(["d/a", "//", "", "b", "/c"]);
+    assert(t1 == false);
+
+    t1 = startsWithSeparator(["d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == false);
+
+    t1 = startsWithSeparator(["d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == false);
+
+    t1 = startsWithSeparator(["", "d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == true);
+}
+
+bool endsWithSeparator(string value)
+{
+    return value.endsWith(dirSeparator);
+}
+
+bool endsWithSeparator(string[] args)
+{
+    return (args.length != 0 && (args[$ - 1] == "" || args[$ - 1].endsWith(dirSeparator)));
+}
+
+unittest
+{
+    auto t1 = endsWithSeparator(["/a", "//", "", "b", "/c"]);
+    assert(t1 == false);
+
+    t1 = endsWithSeparator(["d/a", "//", "", "b", "/c/"]);
+    assert(t1 == true);
+
+    t1 = endsWithSeparator(["d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == true);
+}
+
 /*
 keep_end_empty - if True and *args ends on '' or b'', then result will
 end with trailing slash
 */
-string join(string [] args, bool keep_end_empty=false) {
-    return "123";
+string join(string[] args, bool keep_end_empty = false)
+{
+    auto start_separator = startsWithSeparator(args);
+    auto end_separator = endsWithSeparator(args);
+
+    string ret;
+
+    for (ptrdiff_t i = args.length - 1; i != -1; i += -1)
+    {
+        if (args[i] == "")
+        {
+            args.remove(i);
+        }
+    }
+
+    ret = ret.strip(cast(char[])[dirSeparator]);
+
+    if (start_separator)
+    {
+        ret = dirSeparator ~ ret;
+    }
+
+    if (keep_end_empty && end_separator)
+    {
+        ret ~= dirSeparator;
+    }
+
+    return ret;
+}
+
+unittest
+{
+    auto t1 = join(["/a", "//", "", "b", "/c"]);
+    assert(t1 == "/a/b/c");
+
+    t1 = join(["d/a", "//", "", "b", "/c/"]);
+    assert(t1 == "d/a/b/c/");
+
+    t1 = join(["", "d/a", "//", "", "b", "/c/"]);
+    assert(t1 == " /d/a/b/c/");
+
+    t1 = join([" ", "", "d/a", "//", "", "b", "/c/"]);
+    assert(t1 == " /d/a/b/c/");
+
+    t1 = join(["d/a", "//", "", "b", "/c", ""]);
+    assert(t1 == "d/a/b/c/");
 }
--- source/string.d
+++ source/string.d
@@ -2,85 +2,114 @@ module dutils.string;
 
 import std.string;
 
-ptrdiff_t index(string s, string sub, ptrdiff_t start=0) {
+private ptrdiff_t index(string s, string sub, ptrdiff_t start = 0)
+{
 
     auto sub_length = sub.length;
 
-    while (true) {
-
+    while (true)
+    {
         s = s[start .. $];
 
         auto index = indexOf(s, sub);
-        if (index == -1) {
+        if (index == -1)
+        {
             return -1;
         }
 
-        string s_sub = s[index .. index+sub_length];
-        if (cmp(s_sub, sub) == 0 ){
-            return start+ index;
+        string s_sub = s[index .. index + sub_length];
+        if (cmp(s_sub, sub) == 0)
+        {
+            return start + index;
         }
 
+        start += index;
         start += 1;
     }
-
-    return -1;
 }
 
-unittest {
+unittest
+{
     import std.stdio;
-    import std.format;
 
-    {
-    auto t1=    index("abbc", "bb");
-    assert(t1 == 1, format("must be 1, not %d", t1));
+    auto t1 = index("abbc", "bb");
+    assert(t1 == 1);
 
-    t1=    index("abbc", "d");
-    assert(t1 == -1, format("must be -1, not %d", t1));
+    t1 = index("abbc", "d");
+    assert(t1 == -1);
 
-    t1=    index("abbc", "b");
-    assert(t1 == 1, format("must be 1, not %d", t1));
+    t1 = index("abbc", "b");
+    assert(t1 == 1);
 
-    t1=    index("abbc", "b",2);
-    assert(t1 == 2, format("must be 2, not %d", t1));
+    t1 = index("abbc", "b", 2);
+    assert(t1 == 2);
 
-    t1=    index("abb", "bb");
-    assert(t1 == 1, format("must be 1, not %d", t1));
+    t1 = index("abb", "bb");
+    assert(t1 == 1);
 
-    t1=    index("abb", "bb", 2);
-    assert(t1 == -1, format("must be -1, not %d", t1));
-    }
+    t1 = index("abb", "bb", 2);
+    assert(t1 == -1);
 }
 
-string[] split(string s, string sub) {
+string[] split(string s, string sub)
+{
 
     auto sub_length = sub.length;
 
     string[] ret;
 
-
-    while (true) {
-        auto next_index = index(s, sub);
-        if (next_index == -1) {
-            break;
+    if (sub_length != 0)
+    {
+        while (true)
+        {
+            auto next_index = index(s, sub);
+            if (next_index == -1)
+            {
+                break;
+            }
+            ret ~= s[0 .. next_index];
+            s = s[next_index + sub_length .. $];
         }
-        ret ~= s[0 .. next_index];
-        s = s[next_index+sub_length .. $];
-    }
 
-    ret ~= s;
+        ret ~= s;
+    }
+    else
+    {
+        foreach (char c; s)
+        {
+            ret ~= cast(string)(cast(immutable char[])[c]);
+        }
+    }
 
     return ret;
 }
 
+unittest
+{
+    auto t1 = split("abbc", "bb");
+    assert(t1 == cast(string[])["a", "c"]);
 
-string join(string[] values, string sub) {
+    t1 = split("/", "/");
+    assert(t1 == cast(string[])["", ""]);
+
+    t1 = split("123", "");
+    assert(t1 == cast(string[])["1", "2", "3"]);
+
+    t1 = split("", "");
+    assert(t1 == cast(string[])[]);
+}
+
+string join(string[] values, string sub)
+{
 
     string ret;
     auto values_length = values.length;
 
-    foreach (ptrdiff_t index , string i ; values) {
+    foreach (ptrdiff_t index, string i; values)
+    {
         ret ~= i;
-        if (index < values_length-1) {
+        if (index < values_length - 1)
+        {
             ret ~= sub;
         }
     }
@@ -88,6 +117,20 @@ string join(string[] values, string sub) {
     return ret;
 }
 
- debug {
-    void main() {}
+unittest
+{
+    auto t1 = join(cast(string[])[], "");
+    assert(t1 == "");
+
+    t1 = join(cast(string[])[], "/");
+    assert(t1 == "");
+
+    t1 = join(cast(string[])[""], "/");
+    assert(t1 == "");
+
+    t1 = join(cast(string[])[" "], "/");
+    assert(t1 == " ");
+
+    t1 = join(cast(string[])[" ", " "], "/");
+    assert(t1 == " / ");
 }
