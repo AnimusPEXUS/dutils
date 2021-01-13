9ba04271e91603e4ad8bdc1cb6e2700ff7d38640
Animus PEXUS <animuspexus@protonmail.com>
2021-01-13 18:12:28 +0300

    ic

--- source/dutils/gtkcollection/FileTreeView.d
+++ source/dutils/gtkcollection/FileTreeView.d
@@ -3,6 +3,8 @@ module dutils.gtkcollection.FileTreeView;
 import std.path;
 import std.file;
 import std.algorithm;
+import std.stdio;
+
 /* import std.stdio; */
 
 import gtk.TreeView;
@@ -11,6 +13,7 @@ import gtk.TreeStore;
 import gtk.TreeModelIF;
 import gtk.TreeViewColumn;
 import gtk.TreeIter;
+import gtk.TreePath;
 import gtk.CellRendererPixbuf;
 import gtk.CellRendererText;
 
@@ -62,26 +65,81 @@ class FileTreeView
         }
 
         setRootDirectory(expandTilde("~"));
-    };
+    }
 
-    ScrolledWindow getWidget() {
+    ScrolledWindow getWidget()
+    {
         return tv_sw;
+    }
+
+    TreeView getTreeView()
+    {
+        return tv;
     }
 
     void setRootDirectory(string path)
     {
         rootDir = buildNormalizedPath(path);
         refresh();
-    };
+    }
 
     string getRootDirectory()
     {
         return rootDir;
-    };
+    }
 
-    /* private void listExpanded2() {
+    gulong addOnRowActivated(void delegate(TreePath, TreeViewColumn,
+            TreeView) dlg, ConnectFlags connectFlags = cast(ConnectFlags) 0)
+    {
+        return tv.addOnRowActivated(dlg, connectFlags);
+    }
 
-    }; */
+    /* if FileTreeView finks tp is File */
+    bool isFile(TreePath tp)
+    {
+        return !isDir(tp); // for now. will be changed if FileTreeView will support more file types
+    }
+
+    bool isDir(TreePath tp)
+    {
+        TreeIter ti = new TreeIter;
+        bool ok = cast(bool)(ts.getIter(ti, tp));
+        if (ok)
+        {
+            auto value = ts.getValue(ti, 0);
+            return value.getString() == "folder";
+        }
+        return false;
+    }
+
+    // expands directory relatively to selected root
+    void expandByTreePath(TreePath tp)
+    {
+        auto pth = convertTreePathToFilePath(tp);
+        pth = dutils.path.join([rootDir, pth]);
+        if (std.file.isDir(pth))
+        {
+            TreeIter itr = new TreeIter;
+            ts.getIter(itr, tp);
+            loadDir(itr, pth);
+        }
+    }
+
+    // NOTE: this doesn't adds root path before result
+    string convertTreePathToFilePath(TreePath tp)
+    {
+        auto indices = tp.getIndices();
+        string[] values;
+        /* values ~= rootDir; */
+        for (int i = 0; i != indices.length; i++)
+        {
+            TreeIter value = new TreeIter;
+            auto res = ts.getIter(value, new TreePath(indices[0 .. i + 1]));
+            values ~= ts.getValue(value, 1).getString();
+        }
+        string ret = dutils.path.join(values);
+        return ret;
+    }
 
     void refresh()
     {
@@ -100,35 +158,38 @@ class FileTreeView
             lst ~= baseName(i);
         }
 
-        loop:
-        foreach (i; lst)
+        loop: foreach (i; lst)
         {
             auto joined = dutils.path.join(cast(string[])[path, i]);
-            if ( isSymlink(joined)) {
+            if (isSymlink(joined))
+            {
                 {
 
-                        string link_value=    readLink(joined);
-                        /* string link_value_real = ""; */
+                    string link_value = readLink(joined);
+                    /* string link_value_real = ""; */
 
-                        try {
-                            // TODO: impliment and use realPath() function
-                         auto t = new DirEntry(link_value);
-                        } catch (                            std.file.FileException                            ) {
-                            lst_files ~= i;
-                            continue loop;
-                        }
+                    try
+                    {
+                        // TODO: impliment and use realPath() function
+                        auto t = new DirEntry(link_value);
+                    }
+                    catch (std.file.FileException)
+                    {
+                        lst_files ~= i;
+                        continue loop;
+                    }
 
                 }
             }
 
-                if (isDir(joined))
-                {
-                    lst_dirs ~= i;
-                }
-                else
-                {
-                    lst_files ~= i;
-                }
+            if (std.file.isDir(joined))
+            {
+                lst_dirs ~= i;
+            }
+            else
+            {
+                lst_files ~= i;
+            }
 
         }
 
@@ -138,7 +199,7 @@ class FileTreeView
         auto m = tv.getModel();
 
         {
-            TreeIter chi;
+            TreeIter chi = new TreeIter;
 
             bool res = m.iterChildren(chi, itera);
 
@@ -151,7 +212,7 @@ class FileTreeView
 
         foreach (i; lst_dirs)
         {
-            TreeIter new_iter;
+            TreeIter new_iter = new TreeIter;
             (cast(TreeStore) m).append(new_iter, itera);
             (cast(TreeStore) m).setValuesv(new_iter, [0, 1], [
                     new Value("folder"), new Value(i)
@@ -160,7 +221,7 @@ class FileTreeView
 
         foreach (i; lst_files)
         {
-            TreeIter new_iter;
+            TreeIter new_iter = new TreeIter;
             (cast(TreeStore) m).append(new_iter, itera);
             (cast(TreeStore) m).setValuesv(new_iter, [0, 1], [
                     new Value("txt"), new Value(i)
